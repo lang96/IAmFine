c73852090ab469e0ca9bf13d677159a56c17edf3
darwisy01 <darwisy01@gmail.com>
2021-11-17 15:00:02 +0800

    user meeting(schedule/set meeting) stuffs

--- src/main/java/com/doraemon/iamfine/iamfine/UserHomepageController.java
+++ src/main/java/com/doraemon/iamfine/iamfine/UserHomepageController.java
@@ -13,7 +13,7 @@ import java.io.IOException;
 public class UserHomepageController {
 
     @FXML
-    private Button backBtn;
+    private Button backBtn, meetingBtn;
 
     @FXML
     public void toLandingPage() throws IOException {
@@ -23,5 +23,12 @@ public class UserHomepageController {
         window.getIcons().add(new Image(this.getClass().getResource("/raw/logo.png").toString()));
         window.setScene(new Scene(root,335,602));
 
+    }
+
+    @FXML
+    public void toUserMeetingSchedule() throws IOException{
+        Parent root = FXMLLoader.load(getClass().getResource("userMeetingSchedule.fxml"));
+        Stage window = (Stage) meetingBtn.getScene().getWindow();
+        window.setScene(new Scene(root,335,602));
     }
 }
