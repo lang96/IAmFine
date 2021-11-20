34878d85fda9f4013d2a5685d098ee73fc4d31a8
Abdjb <63862086+Abdjb@users.noreply.github.com>
2021-11-20 21:35:56 +0800

    amik ni shu

--- src/main/java/com/doraemon/iamfine/BindingPageController.java
+++ src/main/java/com/doraemon/iamfine/BindingPageController.java
@@ -5,15 +5,23 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
+import org.w3c.dom.Text;
 
 import java.io.IOException;
 
 public class BindingPageController {
+    @FXML
+    private TextField nameTextField, passwordTextField;
 
     @FXML
-    private Button backBtn;
+    private Label firstName, secondName, thirdName;
+
+    @FXML
+    private Button backBtn, addBtn, Del1Btn, Del2Btn, Del3Btn;
 
     @FXML
     public void toUserProfile() throws IOException {
@@ -22,6 +30,12 @@ public class BindingPageController {
         Stage window = (Stage) backBtn.getScene().getWindow();
         window.getIcons().add(new Image(this.getClass().getResource("/raw/logo.png").toString()));
         window.setScene(new Scene(root,335,602));
+
+    }
+
+    @FXML
+    public void addingUserBtn() throws IOException {
+
 
     }
 }
