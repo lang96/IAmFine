61d316e193815a930b57c59a535ca8ec5943dfaf
Abdjb <63862086+Abdjb@users.noreply.github.com>
2021-11-21 09:09:42 +0800

    add webPage on therapistJoinMeeting.fxml

--- src/main/java/com/doraemon/iamfine/TherapistJoinMeetingController.java
+++ src/main/java/com/doraemon/iamfine/TherapistJoinMeetingController.java
@@ -1,11 +1,15 @@
 package com.doraemon.iamfine;
 
+import javafx.application.Application;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.scene.web.WebView;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -16,6 +20,9 @@ public class TherapistJoinMeetingController {
     private Button backBtn;
 
     @FXML
+    private VBox pane;
+
+    @FXML
     public void toTheraScheduleMeeting() throws IOException {
 
         Parent root = FXMLLoader.load(getClass().getResource("therapistMeeting.fxml"));
@@ -23,5 +30,12 @@ public class TherapistJoinMeetingController {
         window.getIcons().add(new Image(this.getClass().getResource("/raw/logo.png").toString()));
         window.setScene(new Scene(root,335,602));
 
+    }
+
+    @FXML
+    public void initialize(){
+        WebView webView = new WebView();
+        webView.getEngine().load("https://meet.google.com/");
+        pane.getChildren().add(webView);
     }
 }
